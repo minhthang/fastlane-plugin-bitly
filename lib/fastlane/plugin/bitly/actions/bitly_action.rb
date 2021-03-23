e748f8646f1b058a0d754c33639215008ae6d18b
Thang Nguyen <minhthang145@gmail.com>
2021-03-23 17:57:23 +0700

    update config param

--- lib/fastlane/plugin/bitly/actions/bitly_action.rb
+++ lib/fastlane/plugin/bitly/actions/bitly_action.rb
@@ -39,11 +39,16 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "BITLY_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+          FastlaneCore::ConfigItem.new(key: :token,
+                                  env_name: "",
+                               description: "Bitly Access API Token",
+                                  optional: false,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :long_url,
+                                  env_name: "",
+                               description: "Long link",
+                                  optional: false,
+                                      type: String)
         ]
       end
 
@@ -51,7 +56,7 @@ module Fastlane
         # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
         # See: https://docs.fastlane.tools/advanced/#control-configuration-by-lane-and-by-platform
         #
-        # [:ios, :mac, :android].include?(platform)
+        [:ios, :android].include?(platform)
         true
       end
     end
